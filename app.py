9a7bb39caeeb4d21b06815d7c07ce2ec8625b61f
amoshochman <amoshochman1982@gmail.com>
2023-06-20 13:28:02 +0300

    adding csv option

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib
 
-matplotlib.use('agg') #check if in use
+matplotlib.use('agg')  # check if in use
 import matplotlib.pyplot as plt
 from flask import Flask, send_file, jsonify
 import os
@@ -11,6 +11,8 @@ import csv
 from flask_restx import reqparse
 import inspect
 from sqlalchemy.orm.attributes import InstrumentedAttribute
+
+import configparser
 
 from constants import PROJECT_ROOT, TITANIC_DATABASE
 
@@ -23,9 +25,12 @@ TITANIC_DB = 'titanic.db'
 
 app = Flask(__name__)
 api = Api(app)
+csv_dict = {}
 
 
 def as_dict(my_object, attributes_list=None):
+    if type(my_object) == dict:
+        return {k: v for k, v in my_object.items() if (not attributes_list or k in attributes_list)}
     return {c.name: getattr(my_object, c.name) for c in my_object.__table__.columns if
             (not attributes_list or c.name in attributes_list)}
 
@@ -33,6 +38,8 @@ def as_dict(my_object, attributes_list=None):
 @api.route('/passengers')
 class PassengersClass(Resource):
     def get(self):
+        if csv_dict:
+            return jsonify(list(csv_dict.values()))
         return jsonify([as_dict(passenger) for passenger in Passenger.query.all()])
 
 
@@ -80,7 +87,7 @@ class PassengerClass(Resource):
                 return str(e), 400
         else:
             attributes_list = None
-        passenger = Passenger.query.get(passenger_id)
+        passenger = csv_dict[passenger_id] if csv_dict else Passenger.query.get(passenger_id)
         if not passenger:
             return "No passenger found with id: " + str(passenger_id), 400
         return jsonify(as_dict(passenger, attributes_list=attributes_list))
@@ -92,6 +99,39 @@ def create_app(db_location):
     return app
 
 
+def use_db():
+    config = configparser.ConfigParser()
+    try:
+        config.read('config.ini')
+        source = config['DATA']['source']
+        assert source in ('db', 'csv')
+        return True if source == 'db' else False
+    except:
+        raise ValueError(
+            "Invalid configuration given. Expected file config.ini with section DATA['source'] either equal to 'db' or 'csv'")
+
+
+def get_number_or_string(string):
+    try:
+        my_float = float(string)
+        return int(string) if string.isnumeric() else my_float
+    except ValueError:
+        return string
+
+
+def fill_csv_dict():
+    csv_path = os.path.join(CUR_FOLDER, RAW_DATA, TITANIC_CSV)
+    myFile = open(csv_path, 'r')
+    reader = csv.DictReader(myFile)
+    for dictionary in reader:
+        csv_dict[int(dictionary['PassengerId'])] = {k: (get_number_or_string(v) if k != "Ticket" else v) for k, v in
+                                                    dictionary.items()}
+
+
 if __name__ == '__main__':
-    app = create_app(f"sqlite:////{PROJECT_ROOT}/{TITANIC_DATABASE}")
+    if use_db():
+        app = create_app(f"sqlite:////{PROJECT_ROOT}/{TITANIC_DATABASE}")
+    else:
+        fill_csv_dict()
+        # raise ValueError("csv not implemented yet")
     app.run(debug=True)
--- tests/integration/test.py
+++ tests/integration/test.py
@@ -1,4 +1,4 @@
-from app import get_passenger_attributes
+from app import get_passenger_attributes, fill_csv_dict, csv_dict
 from models import Passenger, db
 
 
@@ -6,6 +6,7 @@ def test_passengers(client):
     response = client.get("/passengers")
     assert response.status_code == 200
     assert type(response.json) == list and len(response.json) == 891
+    response_with_db = response.json
     passenger_attributes = get_passenger_attributes()
     assert (all(row.keys() == passenger_attributes for row in response.json))
     name = "Peter Cantropus"
@@ -18,6 +19,16 @@ def test_passengers(client):
     assert (response.json.keys() == passenger_attributes)
     response = client.get("/passenger/" + str(id) + "?attributes=age%20name")
     assert response.json == {"Age": age, "Name":name}
+    some_id = "10"
+    response = client.get("/passenger/" + some_id)
+    response_passenger_with_db = response.json
+    fill_csv_dict()
+    response = client.get("/passengers")
+    response_with_csv = response.json
+    assert response_with_csv == response_with_db
+    response = client.get("/passenger/" + some_id)
+    response_passenger_with_csv = response.json
+    assert response_passenger_with_csv == response_passenger_with_db
 
 
 
